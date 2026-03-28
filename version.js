49f919e8c25f44f998e3d0a74c3c1bcfa8891d0e
lihe-source <lihedalihome@gmail.com>
2026-03-28 09:37:33 +0800

    Add files via upload

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
-const APP_VERSION = 'V4.7';
-const APP_BUILD   = '4_7';
-const CACHE_NAME  = 'cost-record-V4_7';
+const APP_VERSION = 'V4.1';
+const APP_BUILD   = '4_1';
+const CACHE_NAME  = 'cost-record-V4_1';
